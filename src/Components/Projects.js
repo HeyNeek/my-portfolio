e302905a1a482db554bb1a126861b51380c009b5
HeyNeek <nikoaguirre9315@gmail.com>
2024-01-25 19:08:27 -0600

    updated projects page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,40 +1,92 @@
-import React from 'react';
+import React from "react";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.css';
-import spaceCityLogo from '../spacecitylogo.jpg';
-import dndLogo from '../DungeonsAndDragons.jpeg';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.css";
+import spaceCityLogo from "../spacecitylogo.jpg";
+import dndLogo from "../DungeonsAndDragons.jpeg";
 
-function Projects(){
-    return (
-        <Container id="projectsContainer">
-            <h1 id="projectsH1">My Projects</h1>
-            <br/>
-            <Row id="projectsRow">
-                <Card className="projectCards" bg="dark" text="light" border="dark" style={{ margin: '25px', padding: '5px', width: '18rem' }}>
-                    <Card.Img variant="top" src={spaceCityLogo} />
-                    <Card.Body>
-                        <Card.Title><a className="cardLinks" target="_blank" href="https://space-city-coffee.herokuapp.com/">Space City Coffee</a></Card.Title>
-                        <Card.Text>
-                        A site I built that lets you browse through Houston's local coffee shops, create an account, and leave reviews!
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className="projectCards" bg="dark" text="light" border="dark" style={{ padding: '5px', width: '18rem' }}>
-                    <Card.Img variant="top" src={dndLogo} />
-                    <Card.Body>
-                        <Card.Title><a className="cardLinks" target="_blank" href="https://fi-p4-character-creator.herokuapp.com/">DnD Character Creator</a></Card.Title>
-                        <Card.Text>
-                        A site I built with David Evangelista that lets you create an account that will let you create Dungeons & Dragons characters and 
-                        lets you view other people's characters!
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </Row>
-        </Container>
-    )
+function Projects() {
+  return (
+    <Container id="projectsContainer">
+      <h1 className="projectsH1">My Projects</h1>
+      <p className="projects-subheader">
+        (Note: Because Heroku ended the free tier projects I am linking the
+        github repos and the YouTube demo links for some of the projects! :D)
+      </p>
+      <br />
+      <Row id="projectsRow">
+        <Card
+          className="projectCards"
+          bg="dark"
+          text="light"
+          border="dark"
+          style={{ margin: "25px", padding: "5px", width: "18rem" }}
+        >
+          <Card.Img variant="top" src={spaceCityLogo} />
+          <Card.Body>
+            <Card.Title>Space City Coffee</Card.Title>
+            <div className="title-container">
+              <a
+                className="card-links"
+                target="_blank"
+                href="https://www.youtube.com/watch?v=oi2WjwFdS8U&ab_channel=NickAguirre"
+              >
+                Demo
+              </a>
+              <a
+                id="card-github-link"
+                className="card-links"
+                target="_blank"
+                href="https://github.com/HeyNeek/space-city-coffee"
+              >
+                GitHub
+              </a>
+            </div>
+            <Card.Text>
+              A site I built that lets you browse through Houston's local coffee
+              shops, create an account, and leave reviews!
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card
+          className="projectCards"
+          bg="dark"
+          text="light"
+          border="dark"
+          style={{ padding: "5px", width: "18rem" }}
+        >
+          <Card.Img variant="top" src={dndLogo} />
+          <Card.Body>
+            <Card.Title>DnD Character Creator</Card.Title>
+            <div className="title-container">
+              <a
+                className="card-links"
+                target="_blank"
+                href="https://www.youtube.com/watch?v=js5H6XZfNk8&ab_channel=NickAguirre"
+              >
+                Demo
+              </a>
+              <a
+                id="card-github-link"
+                className="card-links"
+                target="_blank"
+                href="https://github.com/HeyNeek/character-creator"
+              >
+                GitHub
+              </a>
+            </div>
+            <Card.Text>
+              A site I built with David Evangelista that lets you create an
+              account that will let you create Dungeons & Dragons characters and
+              lets you view other people's characters!
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Row>
+    </Container>
+  );
 }
 
 export default Projects;
