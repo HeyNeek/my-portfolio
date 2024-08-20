f824bd1461b16c05006dbeddb7f1f6d5afd8f1e3
HeyNeek <nikoaguirre9315@gmail.com>
2024-08-20 13:03:27 -0700

    updated emailJS config

--- src/Components/EmailForm.js
+++ src/Components/EmailForm.js
@@ -1,56 +1,71 @@
-import React, { useRef } from 'react';
+import React, { useRef } from "react";
 
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.css';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.css";
 
+function EmailForm({ setAlertState }) {
+  const form = useRef();
 
-function EmailForm({setAlertState}){
+  function sendEmail(e) {
+    e.preventDefault();
 
-    const form = useRef();
+    emailjs
+      .sendForm("service_50s3v9s", "contact", form.current, "NDGSlV8bm8-nNUGpo")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    form.current.reset();
+    setAlertState(true);
+  }
 
-    function sendEmail(e){
-        e.preventDefault();
+  return (
+    <Form ref={form}>
+      <Form.Group className="mb-3" controlId="formBasicText">
+        <Form.Label>Subject</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter your email's subject"
+          name="subject"
+        />
+      </Form.Group>
 
-        emailjs.sendForm('gmail', 'contact', form.current, 'user_apHMj4JYP6G5G9xt7t7Q9')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        form.current.reset();
-        setAlertState(true);
-    }
+      <Form.Group className="mb-3" controlId="formBasicText">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter your name" name="name" />
+      </Form.Group>
 
-    return (
-        <Form ref={form}>
-            <Form.Group className="mb-3" controlId="formBasicText">
-                <Form.Label>Subject</Form.Label>
-                <Form.Control type="text" placeholder="Enter your email's subject" name="subject" />
-            </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter your email address"
+          name="email"
+        />
+      </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicText">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your name" name="name" />
-            </Form.Group>
-            
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email address" name="email" />
-            </Form.Group>
-            
-            <Form.Group className="mb-3" controlId="formBasicText">
-                <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" rows="5" placeholder="Enter your message here" name="message" />
-            </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicText">
+        <Form.Label>Message</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows="5"
+          placeholder="Enter your message here"
+          name="message"
+        />
+      </Form.Group>
 
-            <Button variant="primary" type="submit" onClick={sendEmail}>
-                Submit
-            </Button>
-        </Form>
-    )
+      <Button variant="primary" type="submit" onClick={sendEmail}>
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
 export default EmailForm;
